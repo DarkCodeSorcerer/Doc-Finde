2f1d3e31b78e21ab0ac6067b61f8c4b56f0bac71
Farshad Inayathulla <143897486+DarkCodeSorcerer@users.noreply.github.com>
2025-04-30 13:35:31 +0530

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-//impliment new line
+//impliment line
 
 test('renders learn react link', () => {
   render(<App />);
